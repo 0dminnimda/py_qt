54711abf764ae06d04109b0e5b362a41352457d0
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-03-20 22:00:00 +0300

    Update nice.py

--- nice.py
+++ nice.py
@@ -2,19 +2,23 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt, QPoint
+import PyQt5
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.wid = 150
+        self.hig = 150
+
         self.setWindowTitle('Example') # имя окна
-        self.setGeometry(50, 50, 500, 500) # рамки
+        self.setGeometry(200, 200, self.wid, self.hig) # рамки
         self.setAttribute(Qt.WA_TranslucentBackground, True) # прозрачные внутренности окна
         self.setWindowFlags(Qt.FramelessWindowHint) # без сноски с именем окна
 
-        self.press = False # ?
-        self.last_pos = QPoint(0, 0) # ?
+        self.press = False # нажато ли окно
+        self.last_pos = QPoint(0, 0) # window pos
 
     def mouseMoveEvent(self, event):
         if self.press:
@@ -32,8 +36,12 @@ class Example(QWidget):
 
     def paintEvent(self, event: QtGui.QPaintEvent):
         painter = QtGui.QPainter(self)
-        painter.setPen(QtGui.QPen(Qt.green, 100))
-        painter.drawRect(self.rect())
+        size = 25
+        painter.setPen(QtGui.QPen(Qt.green, size)) # настройки "кисти"
+        painter.drawEllipse(
+            PyQt5.QtCore.QPoint(self.wid//2, self.hig//2),
+            self.wid/2-size/2, self.hig/2-size/2) # форма окна
+        #print(self.rect().width())
 
 
 if __name__ == '__main__':
