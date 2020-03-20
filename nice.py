a0cf4f9a059c639002f913afd0a78f25ecdd2d7b
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-03-20 22:36:11 +0300

    Update nice.py

--- nice.py
+++ nice.py
@@ -12,7 +12,7 @@ class Example(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        self.text = "asdasdas"
 
         self.wid = 150
         self.hig = 150
@@ -24,14 +24,6 @@ class Example(QWidget):
 
         self.press = False # нажато ли окно
         self.last_pos = QPoint(0, 0) # window pos
-
-    def initUI(self):
-
-        self.text = u'\u041b\u0435\u0432 \u041d\u0438\u043a\u043e\u043b\u0430\
-\u0435\u0432\u0438\u0447 \u0422\u043e\u043b\u0441\u0442\u043e\u0439: \n\
-\u0410\u043d\u043d\u0430 \u041a\u0430\u0440\u0435\u043d\u0438\u043d\u0430'
-
-
 
     def drawText(self, event, qp):
 
