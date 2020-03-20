a0bda88c951ffb9ee3f6054e7fefa95b3e2b7143
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-03-20 22:30:37 +0300

    Update nice.py

--- nice.py
+++ nice.py
@@ -1,13 +1,18 @@
+# -*- coding: utf-8 -*-
+
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5 import QtGui
+from PyQt5.QtGui import QPainter, QColor, QFont
 from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5 import QtGui
 import PyQt5
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.initUI()
 
         self.wid = 150
         self.hig = 150
@@ -19,6 +24,20 @@ class Example(QWidget):
 
         self.press = False # нажато ли окно
         self.last_pos = QPoint(0, 0) # window pos
+
+    def initUI(self):
+
+        self.text = u'\u041b\u0435\u0432 \u041d\u0438\u043a\u043e\u043b\u0430\
+\u0435\u0432\u0438\u0447 \u0422\u043e\u043b\u0441\u0442\u043e\u0439: \n\
+\u0410\u043d\u043d\u0430 \u041a\u0430\u0440\u0435\u043d\u0438\u043d\u0430'
+
+
+
+    def drawText(self, event, qp):
+
+        qp.setPen(QColor(168, 34, 3))
+        qp.setFont(QFont('Decorative', 15))
+        qp.drawText(event.rect(), Qt.AlignCenter, self.text)
 
     def mouseMoveEvent(self, event):
         if self.press:
@@ -36,18 +55,41 @@ class Example(QWidget):
 
     def paintEvent(self, event: QtGui.QPaintEvent):
         painter = QtGui.QPainter(self)
-        size = 25
-        painter.setPen(QtGui.QPen(Qt.green, size)) # настройки "кисти"
-        painter.drawEllipse(
-            PyQt5.QtCore.QPoint(self.wid//2, self.hig//2),
-            self.wid/2-size/2, self.hig/2-size/2) # форма окна
-        #print(self.rect().width())
+        size = 7
+        pos = PyQt5.QtCore.QPoint(self.wid//2, self.hig//2)
+        painter.setPen(QtGui.QPen(Qt.black, size)) # настройки "кисти"
+        painter.drawEllipse(pos, self.wid/2-size, self.hig/2-size) # форма окна
+
+        painter.setPen(QtGui.QPen(Qt.white, size)) # настройки "кисти"
+        painter.drawEllipse(pos, self.wid/2-size*2, self.hig/2-size*2) # форма окна
+        #print(self.pos())
+
+        #qp = QPainter()
+        #qp.begin(self)
+        self.drawText(event, painter)
+        #qp.end()
+
+class mywindow(QMainWindow):
+ 
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Example()
+        self.ui.setupUi(self)
+        
+        self.ui.label.setFont(
+            QtGui.QFont('SansSerif', 30)
+        )
+ 
+        self.ui.label.setGeometry(
+            QtCore.QRect(0, 0, 50, 50)
+        ) # изменить геометрию ярлыка'''
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    w = Example()
-    w.show()
+    wind = Example()
+    #print(Example)
+    wind.show()
 
     sys.exit(app.exec_())
